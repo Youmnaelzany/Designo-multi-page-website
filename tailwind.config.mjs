2102f8189616d75fe47efecf0d775661c9e7e95d
Yaya <youmnael38@gmail.com>
2023-12-10 12:07:33 +0200

    setting up the header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,13 +16,6 @@ module.exports = {
       'white-clr': '##DCDCDC',
       'light-gray-clr': '##DFDFDF',
     },
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
     },
   },
